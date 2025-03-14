65332e3255a179b68a3b3e3183580e51e6fff04e
Gabriel Fernandes <gabrielassuncaofernandes@gmail.com>
2025-03-14 17:31:22 -0300

    Initial Commit

---
+++ academy/academyApp.swift
@@ -0,0 +1,17 @@
+//
+//  academyApp.swift
+//  academy
+//
+//  Created by user on 14/03/25.
+//
+
+import SwiftUI
+
+@main
+struct academyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
