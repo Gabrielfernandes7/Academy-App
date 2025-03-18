c3ab1bddddc0614923b4064ae328f61d4fd2f483
Gabriel Fernandes <gabrielassuncaofernandes@gmail.com>
2025-03-18 17:10:53 -0300

    modify: add NavgationStack in view workout (#2)

--- academy/HomeView.swift
+++ academy/HomeView.swift
@@ -1,15 +1,42 @@
-//
-//  HomeView.swift
-//  ProjetoAcademy
-//
-//  Created by User on 17/03/25.
-//
-
 import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Hello, HomeView!!")
+        NavigationView {
+            List {
+                Section {
+                    // Calendários
+                    Text("Local do calendário")
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                // Item centralizado (título + botão)
+                ToolbarItem(placement: .principal) {
+                    HStack {
+                        Text("Treinos")
+                            .font(.system(size: 30))
+                        Spacer()
+                        Button(action: {
+                            // Ação do botão
+                            print("Botão de adicionar pressionado")
+                        }) {
+                            Image(systemName: "plus")
+                                .foregroundColor(.white)
+                                .padding(10)
+                                .background(Color.blue)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                            Image(systemName: "list.star")
+                                .foregroundColor(.white)
+                                .padding(10)
+                                .background(Color.blue)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
+                    }
+                    .frame(maxWidth: .infinity) // Ocupa toda a largura disponível
+                }
+            }
+        }
     }
 }
 
