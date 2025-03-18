b1608e151e21abb120c89e24d70a320e794d7110
Yuri Elias Alcântara <150107636+YuriElias07@users.noreply.github.com>
2025-03-18 15:42:24 -0300

    feat: onboard and navigation bar (#1)

---
+++ academy/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  ProjetoAcademy
+//
+//  Created by User on 17/03/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, HomeView!!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
+
