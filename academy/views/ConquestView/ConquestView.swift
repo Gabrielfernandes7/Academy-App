2e09491729a78bbefe777b6953aa0b49bc9bed69
Yuri Elias <yuririelias15@gmail.com>
2025-03-21 16:54:19 -0300

    CalendarModel git push origin feature/calendar git commit -m CalendarModel git push origin feature/calendar q
    CalendarHomeView
    
    modify add json
    
    Fix app errors
    
    working mock
    
    filter exercices
    
    add checkbox
    
    ModifyModel

--- academy/views/ConquestView/ConquestView.swift
+++ academy/views/ConquestView/ConquestView.swift
@@ -1,15 +1,34 @@
-//
-//  ConquestView.swift
-//  ProjetoAcademy
-//
-//  Created by User on 18/03/25.
-//
-
 import SwiftUI
 
 struct ConquestView: View {
+    
+    @State private var progress = 75.0
+
     var body: some View {
-        Text("Hello, ConquestView!")
+        NavigationStack {
+            VStack {
+                Text("Conquistas")
+                    .font(.largeTitle)
+                    .padding(.bottom)
+
+                HStack {
+                    Text("3")
+                    ProgressView(value: progress, total: 100)
+                        .progressViewStyle(LinearProgressViewStyle())
+                        .padding(.horizontal, 0)
+                        .frame(height: 10)
+                    Text("4")
+                }
+                .padding(.top, 40)
+
+                Text("Prodígio iniciante")
+                    .font(.title2)
+                    .padding(.top)
+
+                Spacer()
+            }
+            .padding()
+        }
     }
 }
 
--- academy/views/WorkoutView/WorkoutCardView.swift
+++ academy/views/WorkoutView/WorkoutCardView.swift
@@ -8,38 +8,12 @@
 import SwiftUI
 
 struct WorkoutCardView: View {
-    let workout: WorkoutModel
     
     var body: some View {
-        HStack{
-            Image(systemName: "figure.gymnastics").font(.system(size: 30))
-            VStack(alignment: .leading){
-                Text(workout.title)
-                HStack{
-                    Text("\(workout.repetition) rep.")
-                    Text("\(workout.weight)kg")
-                }
-
-            }
-            Spacer()
-            Button {
-                workout.isDone.toggle()
-            } label: {
-                Label {
-                    Text("checkbox")
-                } icon: {
-                    Image(systemName: workout.isDone
-                          ? "checkmark.square.fill"
-                          : "square")
-                }
-                .labelStyle(.iconOnly)
-
-            }
-        }
-
+      Text("asdasd")
     }
 }
 
 #Preview {
-    WorkoutCardView(workout: WorkoutModel(title: "Supino Reto", repetition: "3 x 12 a 15", weight: 40))
+    WorkoutCardView()
 }
